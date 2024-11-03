04d990eb5478de1f1f015407e8922d980cab5f49
gakki-san <gakki.nkk.0331@gmail.com>
2024-11-03 13:46:26 +0900

    chore: pages/のpage.tsx. page.tsをpageファイルとして認識するように変更

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
 };
 
 export default nextConfig;
