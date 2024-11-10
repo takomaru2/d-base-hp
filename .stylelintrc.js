f1ea15341dd989fc4af66d609a87ca93226d3338
gakki-san <gakki.nkk.0331@gmail.com>
2024-11-10 14:45:21 +0900

    docs: stylelintのrulesにコメント追加

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: ['stylelint-config-recess-order', 'stylelint-config-standard-scss'],
   plugins: ['stylelint-scss'],
   rules: {
+    // css変数名をキャメルケース適応
     'scss/dollar-variable-pattern': '^[a-z][a-zA-Z]*',
   },
 };
