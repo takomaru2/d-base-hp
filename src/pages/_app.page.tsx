52cbd9241d0e0d8cc88e51873c6675a82ec58b1c
gakki-san <gakki.nkk.0331@gmail.com>
2024-11-10 22:45:47 +0900

    feat: global.scssを作成して、backgroundcolorをつけました

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import 'ress';
+import '../styles/globals.scss';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
