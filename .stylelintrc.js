31a3fab45509cb51dda4229e0c4a55c0a3566ab4
trptz <tiropetz@gmail.com>
2024-11-11 14:40:27 +0900

    chore: selector-class-patternのdry化

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,9 +1,11 @@
+const lowerCamel = '^[a-z][a-zA-Z]*';
+
 module.exports = {
   extends: ['stylelint-config-recess-order', 'stylelint-config-standard-scss'],
   plugins: ['stylelint-scss'],
   rules: {
     // css変数名をキャメルケース適応
-    'scss/dollar-variable-pattern': '^[a-z][a-zA-Z]*',
-    'selector-class-pattern': '^[a-z][a-zA-Z]*',
+    'scss/dollar-variable-pattern': lowerCamel,
+    'selector-class-pattern': lowerCamel,
   },
 };
