141ede781e8251988c562c9b807b9be533074cf1
gakki-san <gakki.nkk.0331@gmail.com>
2024-11-12 01:01:11 +0900

    feat: storybook化とcolorpropsの追加

--- src/components/SectionTitle/index.tsx
+++ src/components/SectionTitle/index.tsx
@@ -1,16 +1,23 @@
-import styles from '@/components/SectionTitle/index.module.scss';
+import styles from './index.module.scss';
 import { FC } from 'react';
 
 type SectionTitleProps = {
   jpTitle: string;
   enTitle: string;
+  color: string;
 };
 
-export const SectionTitle: FC<SectionTitleProps> = ({ jpTitle, enTitle }) => {
+export const SectionTitle: FC<SectionTitleProps> = ({
+  jpTitle,
+  enTitle,
+  color,
+}) => {
+  const sectionColor = color === 'white' ? styles.white : styles.black;
+
   return (
-    <div className={styles.wrapper}>
+    <div className={`${styles.wrapper} ${sectionColor}`}>
       <h2 className={styles.jpTitle}>{jpTitle}</h2>
-      <div className={styles.border}></div>
+      <div className={`${styles.border} ${sectionColor}`}></div>
       <div className={styles.enTitle}>{enTitle}</div>
     </div>
   );
