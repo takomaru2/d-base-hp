f73e1bf896327f2f2e7293ce8afa07bfaf36867d
gakki-san <gakki.nkk.0331@gmail.com>
2024-11-12 22:35:17 +0900

    feat: HeaderPropsを渡して、storybook化

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,7 +7,7 @@ type HeaderProps = {
   isOpenMenu: boolean;
 };
 
-export const Header: FC<HeaderProps> = () => {
+export const Header: FC<HeaderProps> = ({ isOpenMenu }) => {
   return (
     <div className={styles.container}>
       <div className={styles.logo}>
@@ -35,32 +35,34 @@ export const Header: FC<HeaderProps> = () => {
             <Link href="/">お問い合わせ</Link>
           </li>
         </ul>
-        <div className={styles.navMenu}>
-          <div className={styles.top}>
-            <Logo />
-            <button className={styles.navClose}>X</button>
+        {isOpenMenu && (
+          <div className={styles.navMenu}>
+            <div className={styles.top}>
+              <Logo />
+              <button className={styles.navClose}>×</button>
+            </div>
+            <ul className={styles.navMenuList}>
+              <li className={styles.navMenuItem}>
+                <Link href="/">ホーム</Link>
+              </li>
+              <li className={styles.navMenuItem}>
+                <Link href="/">施工実績</Link>
+              </li>
+              <li className={styles.navMenuItem}>
+                <Link href="/">価格表</Link>
+              </li>
+              <li className={styles.navMenuItem}>
+                <Link href="/">アクセス</Link>
+              </li>
+              <li className={styles.navMenuItem}>
+                <Link href="/">よくある質問</Link>
+              </li>
+              <li className={styles.navMenuItem}>
+                <Link href="/">お問い合わせ</Link>
+              </li>
+            </ul>
           </div>
-          <ul className={styles.navMenuList}>
-            <li className={styles.navMenuItem}>
-              <Link href="/">ホーム</Link>
-            </li>
-            <li className={styles.navMenuItem}>
-              <Link href="/">施工実績</Link>
-            </li>
-            <li className={styles.navMenuItem}>
-              <Link href="/">価格表</Link>
-            </li>
-            <li className={styles.navMenuItem}>
-              <Link href="/">アクセス</Link>
-            </li>
-            <li className={styles.navMenuItem}>
-              <Link href="/">よくある質問</Link>
-            </li>
-            <li className={styles.navMenuItem}>
-              <Link href="/">お問い合わせ</Link>
-            </li>
-          </ul>
-        </div>
+        )}
       </div>
     </div>
   );
