0232d129b15e3e18421dffe4eb0184af07e229d5
gakki-san <gakki.nkk.0331@gmail.com>
2024-11-12 22:18:50 +0900

    feat: navメニューとspMENUにスタイルを当てた

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,13 @@
 import { Logo } from '../Logo';
 import styles from './index.module.scss';
 import Link from 'next/link';
+import { FC } from 'react';
 
-export const Header = () => {
+type HeaderProps = {
+  isOpenMenu: boolean;
+};
+
+export const Header: FC<HeaderProps> = () => {
   return (
     <div className={styles.container}>
       <div className={styles.logo}>
@@ -30,6 +35,32 @@ export const Header = () => {
             <Link href="/">お問い合わせ</Link>
           </li>
         </ul>
+        <div className={styles.navMenu}>
+          <div className={styles.top}>
+            <Logo />
+            <button className={styles.navClose}>X</button>
+          </div>
+          <ul className={styles.navMenuList}>
+            <li className={styles.navMenuItem}>
+              <Link href="/">ホーム</Link>
+            </li>
+            <li className={styles.navMenuItem}>
+              <Link href="/">施工実績</Link>
+            </li>
+            <li className={styles.navMenuItem}>
+              <Link href="/">価格表</Link>
+            </li>
+            <li className={styles.navMenuItem}>
+              <Link href="/">アクセス</Link>
+            </li>
+            <li className={styles.navMenuItem}>
+              <Link href="/">よくある質問</Link>
+            </li>
+            <li className={styles.navMenuItem}>
+              <Link href="/">お問い合わせ</Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
