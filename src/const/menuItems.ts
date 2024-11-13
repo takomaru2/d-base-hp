3c391401a63ac5f6bb048215bd2cf6ae1514bf1e
gakki-san <gakki.nkk.0331@gmail.com>
2024-11-13 19:05:45 +0900

    refactor: menuItemsにてmenuItemの型を定義

--- src/const/menuItems.ts
+++ src/const/menuItems.ts
@@ -1,4 +1,6 @@
-export const menuItems = [
+type MenuItem = { id: string; href: string; label: string };
+
+export const menuItems: MenuItem[] = [
   { id: '1', href: '/', label: 'ホーム' },
   { id: '2', href: '/', label: '施工実績' },
   { id: '3', href: '/', label: '価格表' },
