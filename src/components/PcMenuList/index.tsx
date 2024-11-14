5d18a41c6cf4004884874e1cc52aab3dada316d4
gakki-san <gakki.nkk.0331@gmail.com>
2024-11-14 14:40:18 +0900

    refactor: コンポーネント化したため、命名をシンプルに変更

--- src/components/PcMenuList/index.tsx
+++ src/components/PcMenuList/index.tsx
@@ -5,7 +5,7 @@ import { FC } from 'react';
 
 export const PcMenuList: FC = () => {
   return (
-    <ul className={styles.pcMenuList}>
+    <ul className={styles.container}>
       {menuItems.map((item) => (
         <li key={item.id} className={styles.menuItem}>
           <Link href={item.href}>{item.label}</Link>
