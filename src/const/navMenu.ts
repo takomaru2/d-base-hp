5e09756e0f9b099be5fed0177cb83d9aae5778f3
gakki-san <gakki.nkk.0331@gmail.com>
2024-11-14 21:14:40 +0900

    refactor: todoコメント追加

--- src/const/navMenu.ts
+++ src/const/navMenu.ts
@@ -1,6 +1,7 @@
 type NavMenuList = { id: string; href: string; label: string };
 
 export const navMenu: NavMenuList[] = [
+  // todo: リンク先入れる
   { id: '1', href: '/', label: 'ホーム' },
   { id: '2', href: '/', label: '施工実績' },
   { id: '3', href: '/', label: '価格表' },
