31a6c9504fc61232f4583075bb156166a0ff930c
gakki-san <gakki.nkk.0331@gmail.com>
2024-11-18 17:21:01 +0900

    refactor: 💡 コメントの追加

--- src/hooks/useAutoSlide.ts
+++ src/hooks/useAutoSlide.ts
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react';
 
+// 引数にスライド枚数を渡してあげることで、何枚でもループするようになるよ。lengthを指定してね。
 export const useAutoSlide = (length: number, interval: number = 4000) => {
   const [activeIndex, setActiveIndex] = useState(0);
 
-  // コンポーネントがマウントされるたびに呼び出し、前回のアニメーションから4秒経っていればactiveIndexを更新する
+  // コンポーネントがマウントされるたびに呼び出し、前回のアニメーションから4秒経っていればactiveIndexを更新する。intervalはデフォルトで4秒設定
   useEffect(() => {
     let lastTime = performance.now();
     let cancelId: number;
