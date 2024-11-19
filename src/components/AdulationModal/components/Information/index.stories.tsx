8d12ee5afb6142ed35b5b85dc9d9e67da190d61d
gakki-san <gakki.nkk.0331@gmail.com>
2024-11-19 13:24:03 +0900

    refactor: 💡 infomation部分をコンポーネント化、storybook化

---
+++ src/components/AdulationModal/components/Information/index.stories.tsx
@@ -0,0 +1,13 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import { Information as Component } from '.';
+
+const meta: Meta<typeof Component> = {
+  component: Component,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Component>;
+
+export const Default: Story = {};
---
+++ src/components/AdulationModal/components/Information/index.tsx
@@ -0,0 +1,11 @@
+import styles from './index.module.scss';
+import { FC } from 'react';
+
+export const Information: FC = () => {
+  return (
+    <ul className={styles.information}>
+      <li className={styles.time}>営業時間 : 10:00 ~ 19:00</li>
+      <li className={styles.closing}>定休日 : 毎週水曜日</li>
+    </ul>
+  );
+};
