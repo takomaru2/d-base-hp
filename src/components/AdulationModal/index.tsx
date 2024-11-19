c6298bcbb2bd75f0821ab1f24c8861fdcde9ebb6
gakki-san <gakki.nkk.0331@gmail.com>
2024-11-19 13:25:40 +0900

    refactor: 💡 telLinkをコンポーネント、storybook化

--- src/components/AdulationModal/index.tsx
+++ src/components/AdulationModal/index.tsx
@@ -1,26 +1,15 @@
 import styles from './index.module.scss';
-import Image from 'next/image';
-import line from '@/../public/assets/top/line.png';
-import Link from 'next/link';
+import { FC } from 'react';
+import { TelLink } from '@/components/AdulationModal/components/TelLink';
+import { LineLink } from '@/components/AdulationModal/components/LineLink';
+import { Information } from '@/components/AdulationModal/components/Information';
 
-export const AdulationModal = () => {
+export const AdulationModal: FC = () => {
   return (
     <div className={styles.container}>
-      <address className={styles.tel}>
-        <a href="tel:0859-00-0000">TEL: 0859-00-0000</a>
-      </address>
-      <Link href="https://line.me/ja/" className={styles.line}>
-        <Image className={styles.icon} src={line} alt="Lineのアイコン" />
-        <p className={styles.guide}>
-          <span className={styles.green}>LINE</span>で
-          <br />
-          のお問い合わせはこちらから
-        </p>
-      </Link>
-      <ul className={styles.information}>
-        <li className={styles.time}>営業時間 : 10:00 ~ 19:00</li>
-        <li className={styles.closing}>定休日 : 毎週水曜日</li>
-      </ul>
+      <TelLink />
+      <LineLink />
+      <Information />
     </div>
   );
 };
