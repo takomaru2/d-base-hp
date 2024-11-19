3369820e0effdb1a6e278a4c0836ef216ffb50b3
gakki-san <gakki.nkk.0331@gmail.com>
2024-11-19 13:24:54 +0900

    refactor: 💡 LineLinkをコンポーネント、storybook化

---
+++ src/components/AdulationModal/components/LineLink/index.tsx
@@ -0,0 +1,18 @@
+import styles from './index.module.scss';
+import Image from 'next/image';
+import line from '@/../public/assets/top/line.png';
+import Link from 'next/link';
+import { FC } from 'react';
+
+export const LineLink: FC = () => {
+  return (
+    <Link href="https://line.me/ja/" className={styles.line}>
+      <Image className={styles.icon} src={line} alt="Lineのアイコン" />
+      <p className={styles.guide}>
+        <span className={styles.green}>LINE</span>で
+        <br />
+        のお問い合わせはこちらから
+      </p>
+    </Link>
+  );
+};
