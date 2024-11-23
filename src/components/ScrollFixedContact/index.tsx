cd8d0b3dd31132121aa2d09291925bd7b91a79f8
gakki-san <gakki.nkk.0331@gmail.com>
2024-11-23 21:00:28 +0900

    refactor: 💡 他で再利用しないコンポーネントを一つにまとめた

--- src/components/ScrollFixedContact/index.tsx
+++ src/components/ScrollFixedContact/index.tsx
@@ -1,15 +1,27 @@
-import styles from './index.module.scss';
 import { FC } from 'react';
-import { TelLink } from '@/components/ScrollFixedContact/components/TelLink';
-import { LineLink } from '@/components/ScrollFixedContact/components/LineLink';
-import { ShopSchedule } from '@/components/ScrollFixedContact/components/ShopSchedule';
+import Link from 'next/link';
+import Image from 'next/image';
+import line from '@/../public/assets/top/line.png';
+import styles from './index.module.scss';
 
 export const ScrollFixedContact: FC = () => {
   return (
     <div className={styles.container}>
-      <TelLink />
-      <LineLink />
-      <ShopSchedule />
+      <address className={styles.tel}>
+        <a href="tel:0859-00-0000">TEL: 0859-00-0000</a>
+      </address>
+      <Link href="https://line.me/ja/" className={styles.line}>
+        <Image className={styles.icon} src={line} alt="LINE" />
+        <div className={styles.guide}>
+          <span className={styles.green}>LINE</span>で
+          <br />
+          のお問い合わせはこちらから
+        </div>
+      </Link>
+      <ul className={styles.scheduleList}>
+        <li className={styles.time}>営業時間 : 10:00 ~ 19:00</li>
+        <li className={styles.closing}>定休日 : 毎週水曜日</li>
+      </ul>
     </div>
   );
 };
