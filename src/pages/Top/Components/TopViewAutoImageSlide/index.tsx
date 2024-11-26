29f291782d8a342c9e4af38ed45bb8292dd3a66a
gakki-san <gakki.nkk.0331@gmail.com>
2024-11-26 14:06:04 +0900

    refactor: 💡topViewSliderでhooks化したanimationframeをインポート

--- src/pages/Top/Components/TopViewAutoImageSlide/index.tsx
+++ src/pages/Top/Components/TopViewAutoImageSlide/index.tsx
@@ -2,8 +2,8 @@ import { TopViewImage } from '@/pages/top/const/topViewImageList';
 import styles from './index.module.scss';
 import { getImageStateIndex } from '@/pages/top/logics/getImageStateIndex';
 import Image from 'next/image';
-import { FC } from 'react';
-import { useAutoSlide } from '@/hooks/useAutoSlide';
+import { FC, useState } from 'react';
+import { useAnimationFrameInterval } from '@/hooks/useAnimationFrameInterval';
 
 type TopViewAutoImageSlideProps = {
   topViewImageList: TopViewImage[];
@@ -12,7 +12,13 @@ type TopViewAutoImageSlideProps = {
 export const TopViewAutoImageSlide: FC<TopViewAutoImageSlideProps> = ({
   topViewImageList,
 }) => {
-  const activeIndex = useAutoSlide(topViewImageList.length);
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const length = topViewImageList.length;
+  useAnimationFrameInterval(() => {
+    setActiveIndex((previousIndex) => (previousIndex + 1) % length);
+  }, 4000);
+
   return (
     <>
       {topViewImageList.map((item, index) => (
