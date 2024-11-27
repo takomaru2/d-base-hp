5c0c28ef42e41de44650f80b1bafab198cfcc87c
takomaru2 <col.dd93@gmail.com>
2024-11-27 12:09:58 +0900

    feat: 🎸 PCのレスポンシブ対応

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,36 +5,32 @@ import { FC } from 'react';
 export const Footer: FC = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.menu}>
-        <div className={styles.flexLeftMenu}>
-          <li>
-            <a href="home">ホーム</a>
-          </li>
-          <li>
-            <a href="price">価格表</a>
-          </li>
-          <li>
-            <a href="works">施工実績</a>
-          </li>
-          <li>
-            <a href="privacy">プライバシーポリシー</a>
-          </li>
-        </div>
-        <div className={styles.flexRightMenu}>
-          <li>
-            <a href="form">お問い合わせ</a>
-          </li>
-          <li>
-            <a href="faq">よくある質問</a>
-          </li>
-          <li>
-            <a href="access">アクセス</a>
-          </li>
-        </div>
-      </div>
-      <div className={styles.logo}>
+      <ul className={styles.menu}>
+        <li className={styles.navMenu}>
+          <a href="home">ホーム</a>
+        </li>
+        <li className={styles.navMenu}>
+          <a href="price">価格表</a>
+        </li>
+        <li className={styles.navMenu}>
+          <a href="works">施工実績</a>
+        </li>
+        <li className={styles.navMenu}>
+          <a href="form">お問い合わせ</a>
+        </li>
+        <li className={styles.navMenu}>
+          <a href="faq">よくある質問</a>
+        </li>
+        <li className={styles.navMenu}>
+          <a href="access">アクセス</a>
+        </li>
+        <li className={styles.navMenu}>
+          <a href="privacy">プライバシーポリシー</a>
+        </li>
+      </ul>
+      <a href="logo" className={styles.logoLink}>
         <Logo color="white" />
-      </div>
+      </a>
     </div>
   );
 };
