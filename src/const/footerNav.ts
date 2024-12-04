8293a36865e728703b1bcf1c091f958fe5116923
takomaru2 <col.dd93@gmail.com>
2024-12-04 16:56:23 +0900

    feat: 🎸 footerで使うnavigationたちをconstファイルに書き出した

---
+++ src/const/footerNav.ts
@@ -0,0 +1,12 @@
+type FooterNavList = { id: string; href: string; label: string };
+
+export const footerNav: FooterNavList[] = [
+  // todo: リンク先入れる
+  { id: '1', href: '/', label: 'ホーム' },
+  { id: '2', href: '/', label: '価格表' },
+  { id: '3', href: '/', label: '施工実績' },
+  { id: '4', href: '/', label: 'お問い合わせ' },
+  { id: '5', href: '/', label: 'よくある質問' },
+  { id: '6', href: '/', label: 'アクセス' },
+  { id: '7', href: '/', label: 'プライバシポリシー' },
+];
