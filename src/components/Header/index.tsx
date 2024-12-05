c51ad6da602deb1cf5714c786e9cf4399e5ffa90
gakki-san <gakki.nkk.0331@gmail.com>
2024-12-05 16:56:19 +0900

    refactor: 💡 Headerコンポーネントをpropsなしで置くだけで表示できるように変更

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,22 +3,21 @@ import styles from './index.module.scss';
 import { FC } from 'react';
 import { SpNavMenu } from '../SpNavMenu';
 import { PcNavMenu } from '../PcNavMenu';
+import { useIsBoolean } from '@/logics/useBoolean';
 
-type HeaderProps = {
-  isOpenMenu: boolean;
-  close: () => void;
-};
-
-export const Header: FC<HeaderProps> = ({ isOpenMenu, close }) => {
+export const Header: FC = () => {
+  const [isOpenMenu, { toggle }] = useIsBoolean();
   return (
     <div className={styles.container}>
       <div className={styles.logo}>
         <Logo color="white" />
       </div>
       <div className={styles.menu}>
-        <button className={styles.spMenu}>MENU</button>
+        <button className={styles.spMenu} onClick={toggle}>
+          MENU
+        </button>
         <PcNavMenu />
-        {isOpenMenu && <SpNavMenu close={close} />}
+        {isOpenMenu && <SpNavMenu close={toggle} />}
       </div>
     </div>
   );
