2e4ed3161adde973ebccd4d38d5d3619d4611a6d
takomaru2 <col.dd93@gmail.com>
2024-12-10 22:20:58 +0900

    feat: 🎸 component化した

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,32 +1,17 @@
 import { Logo } from '../Logo';
 import styles from './index.module.scss';
 import { FC } from 'react';
+import { footerNav } from '@/const/footerNav';
 
 export const Footer: FC = () => {
   return (
     <div className={styles.container}>
       <ul className={styles.menu}>
-        <li className={styles.navMenu}>
-          <a href="home">ホーム</a>
-        </li>
-        <li className={styles.navMenu}>
-          <a href="price">価格表</a>
-        </li>
-        <li className={styles.navMenu}>
-          <a href="works">施工実績</a>
-        </li>
-        <li className={styles.navMenu}>
-          <a href="form">お問い合わせ</a>
-        </li>
-        <li className={styles.navMenu}>
-          <a href="faq">よくある質問</a>
-        </li>
-        <li className={styles.navMenu}>
-          <a href="access">アクセス</a>
-        </li>
-        <li className={styles.navMenu}>
-          <a href="privacy">プライバシーポリシー</a>
-        </li>
+        {footerNav.map((item) => (
+          <li key={item.href} className={styles.footerNav}>
+            <a href={item.href}>{item.name}</a>
+          </li>
+        ))}
       </ul>
       <a href="logo" className={styles.logoLink}>
         <Logo color="white" />
