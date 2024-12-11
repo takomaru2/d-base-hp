cbe47d73c2b928961e80ff6ffcebae2981a11de3
gakki-san <gakki.nkk.0331@gmail.com>
2024-12-11 12:13:18 +0900

    refactor: 💡 breakpointのサイズをconstにまとめた

--- src/pages/Top/hooks/useWorksSliderBreakPoints.ts
+++ src/pages/Top/hooks/useWorksSliderBreakPoints.ts
@@ -9,34 +9,42 @@ export type UseWorksSliderBreakPoints = {
   gap: number;
 };
 
-export const useWorksSliderBreakPoints = () => {
+// todo: review開発時に、同じ条件でよさそうなら、constに切り出して共通化
+const pc = {
+  basicWidth: 250,
+  heroWidth: 500,
+  basicHeight: 355,
+  heroHeight: 710,
+  gap: 50,
+};
+
+const tablet = {
+  basicWidth: 140,
+  heroWidth: 300,
+  basicHeight: 213,
+  heroHeight: 450,
+  gap: 25,
+};
+
+const sp = {
+  basicWidth: 78,
+  heroWidth: 166,
+  basicHeight: 122,
+  heroHeight: 259,
+  gap: 10,
+};
+
+export const useWorksSliderBreakPoints = (): UseWorksSliderBreakPoints => {
   const windowWidth = useWindowWidth();
 
-  const pc = windowWidth > PC;
-  const tablet = windowWidth > TABLET;
-  if (pc) {
-    return {
-      basicWidth: 250,
-      heroWidth: 500,
-      basicHeight: 355,
-      heroHeight: 710,
-      gap: 50,
-    };
-  } else if (tablet) {
-    return {
-      basicWidth: 140,
-      heroWidth: 300,
-      basicHeight: 213,
-      heroHeight: 450,
-      gap: 25,
-    };
+  const pcBreakPoint = windowWidth > PC;
+  const tabletBreakPoint = windowWidth > TABLET;
+
+  if (pcBreakPoint) {
+    return pc;
+  } else if (tabletBreakPoint) {
+    return tablet;
   } else {
-    return {
-      basicWidth: 78,
-      heroWidth: 166,
-      basicHeight: 122,
-      heroHeight: 259,
-      gap: 10,
-    };
+    return sp;
   }
 };
