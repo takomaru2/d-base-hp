c2e97552082d18ea7a6f2b20f381a87250cd89a9
takomaru2 <col.dd93@gmail.com>
2024-12-16 14:49:25 +0900

    refactor: 💡 h1 タグを Link タグで囲んで、ロゴをクリック可能にした

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import styles from './index.module.scss';
+import Link from 'next/link';
 
 type LogoProps = {
   color?: 'black' | 'white';
@@ -7,5 +8,9 @@ type LogoProps = {
 
 export const Logo: FC<LogoProps> = ({ color = 'white' }) => {
   const colorStyle = color === 'white' ? styles.colorWhite : styles.colorBlack;
-  return <h1 className={`${styles.heading} ${colorStyle}`}>D-base</h1>;
+  return (
+    <Link href={'/'} className={styles.logoLink}>
+      <h1 className={`${styles.heading} ${colorStyle}`}>D-base</h1>
+    </Link>
+  );
 };
