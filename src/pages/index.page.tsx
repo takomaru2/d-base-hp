bdfb71b1c78cfdcacca80b0f7fbec01677950578
gakki-san <gakki.nkk.0331@gmail.com>
2024-12-19 17:54:15 +0900

    feat: 🎸 create price section style

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,3 +1,12 @@
+import { Logo } from '@/components/Logo';
+import Image from 'next/image';
+import priceImage from '@/../public/assets/top/price/priceSectionBackImage.jpg';
+
 export default function Top() {
-  return <></>;
+  return (
+    <>
+      <Logo />
+      <Image src={priceImage} alt={''} width={300} height={300} />
+    </>
+  );
 }
---
+++ src/pages/top/components/PriceSection/index.tsx
@@ -0,0 +1,38 @@
+import { PricingPlan } from '@/pages/top/components/PricingPlan';
+import { SectionTitle } from '@/components/SectionTitle';
+import { Button } from '@/pages/top/components/Button';
+import styles from './index.module.scss';
+
+export const PriceSection = () => {
+  return (
+    <div
+      className={styles.container}
+      style={{
+        backgroundImage: `url("/assets/top/price/priceSectionBackImage.jpg")`,
+      }}
+    >
+      <div className={styles.sectionTitle}>
+        <SectionTitle jpTitle={'価格表'} enTitle={'Price'} color={'white'} />
+      </div>
+      <div className={styles.planWrapper}>
+        <PricingPlan
+          title={'新車〜登録３ヶ月'}
+          description={
+            '低価格でコーティングの効果を重視するあなたにおすすめ。ボディ全面を塗装の負担なく研磨します.'
+          }
+          minPrice={'104,500'}
+        />
+        <PricingPlan
+          title={'経年車'}
+          description={
+            '最高の艶が出るまで無制限に研磨します。ボディ全面はもちろんピラーやドア内まで徹底的に磨きます。'
+          }
+          minPrice={'127,000'}
+        />
+        <div className={styles.button}>
+          <Button label={'詳しい価格表を見る'} />
+        </div>
+      </div>
+    </div>
+  );
+};
