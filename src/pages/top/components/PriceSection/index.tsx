cd9dfee4b7767183d5dc3d696231e1af0a677c11
gakki-san <gakki.nkk.0331@gmail.com>
2024-12-20 10:39:58 +0900

    feat: 🎸 planListをmapして表示。color変数の使用

--- src/pages/top/components/PriceSection/index.tsx
+++ src/pages/top/components/PriceSection/index.tsx
@@ -2,6 +2,7 @@ import { PricingPlan } from '@/pages/top/components/PricingPlan';
 import { SectionTitle } from '@/components/SectionTitle';
 import { Button } from '@/pages/top/components/Button';
 import styles from './index.module.scss';
+import { pricingPlanList } from '@/pages/top/const/praicingPlanList';
 
 export const PriceSection = () => {
   return (
@@ -15,20 +16,14 @@ export const PriceSection = () => {
         <SectionTitle jpTitle={'価格表'} enTitle={'Price'} color={'white'} />
       </div>
       <div className={styles.planWrapper}>
-        <PricingPlan
-          title={'新車〜登録３ヶ月'}
-          description={
-            '低価格でコーティングの効果を重視するあなたにおすすめ。ボディ全面を塗装の負担なく研磨します.'
-          }
-          minPrice={'104,500'}
-        />
-        <PricingPlan
-          title={'経年車'}
-          description={
-            '最高の艶が出るまで無制限に研磨します。ボディ全面はもちろんピラーやドア内まで徹底的に磨きます。'
-          }
-          minPrice={'127,000'}
-        />
+        {pricingPlanList.map((item) => (
+          <PricingPlan
+            key={item.id}
+            title={item.title}
+            description={item.description}
+            minPrice={item.minPrice}
+          />
+        ))}
         <div className={styles.button}>
           <Button
             label={'詳しい価格表を見る'}
