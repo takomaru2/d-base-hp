02c861e71ef94f04b37e7a0722219a44357d2e9b
gakki-san <gakki.nkk.0331@gmail.com>
2024-12-20 11:19:01 +0900

    refactor: 💡 テストコードの削除

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,12 +1,3 @@
-import { Logo } from '@/components/Logo';
-import Image from 'next/image';
-import priceImage from '@/../public/assets/top/price/priceSectionBackImage.jpg';
-
 export default function Top() {
-  return (
-    <>
-      <Logo />
-      <Image src={priceImage} alt={''} width={300} height={300} />
-    </>
-  );
+  return <></>;
 }
