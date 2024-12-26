0a81d21a19ffd691f5baa0ffd83ca0440542e4b8
gakki-san <gakki.nkk.0331@gmail.com>
2024-12-26 17:11:12 +0900

    refactor: 💡 imageをconstに切り出し

--- src/pages/Top/Components/FAQItem/index.tsx
+++ src/pages/Top/Components/FAQItem/index.tsx
@@ -1,5 +1,8 @@
 import React, { FC } from 'react';
 import styles from './index.module.scss';
+import plusImage from '../../../../../public/assets/top/plus.png';
+import minusImage from '../../../../../public/assets/top/minus.png';
+import Image from 'next/image';
 
 type FAQItemProps = {
   item: { id: string; question: string; answer: string };
@@ -7,17 +10,26 @@ type FAQItemProps = {
   isOpen: boolean;
 };
 
+const minusIcon = (
+  <Image src={minusImage} alt={'minus icon'} className={styles.icon} />
+);
+
+const plusIcon = (
+  <Image src={plusImage} alt={'plus icon'} className={styles.icon} />
+);
+
 export const FAQItem: FC<FAQItemProps> = ({ item, onClick, isOpen }) => {
+  const icon = isOpen ? minusIcon : plusIcon;
   return (
     <li className={styles.block}>
       <button className={styles.question} onClick={onClick}>
-        <div className={styles.questionIcon}>Q</div>
+        <div className={styles.questionMark}>Q</div>
         <p>{item.question}</p>
-        <div>{isOpen ? '-' : '+'}</div>
+        <div>{icon}</div>
       </button>
       {isOpen && (
         <div className={styles.answer}>
-          <div className={styles.answerIcon}>A</div>
+          <div className={styles.answerMark}>A</div>
           <p>{item.answer}</p>
         </div>
       )}
