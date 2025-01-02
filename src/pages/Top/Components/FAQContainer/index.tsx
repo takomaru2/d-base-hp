5a1c503a8a57a210a77ac814104a7896cff5690f
gakki-san <gakki.nkk.0331@gmail.com>
2025-01-02 10:00:29 +0900

    refactor: 💡 useFAQの戻り値の変数を変更しました

--- src/pages/Top/Components/FAQContainer/index.tsx
+++ src/pages/Top/Components/FAQContainer/index.tsx
@@ -5,7 +5,7 @@ import { useFAQ } from '@/pages/top/hooks/useFAQ';
 import { FAQItem } from '@/pages/top/components/FAQItem';
 
 export const FAQContainer: FC = () => {
-  const { isAnswerVisible: isOpen, getActiveIndex: onClick } = useFAQ();
+  const { isAnswerVisible, getActiveIndex } = useFAQ();
 
   return (
     <ul className={styles.container}>
@@ -13,8 +13,8 @@ export const FAQContainer: FC = () => {
         <FAQItem
           key={item.id}
           item={item}
-          onClick={onClick(index)}
-          isOpen={isOpen(index)}
+          onClick={getActiveIndex(index)}
+          isOpen={isAnswerVisible(index)}
         />
       ))}
     </ul>
