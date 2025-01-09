3b626322a269239ddeb20bf194209b6fe053f47c
gakki-san <gakki.nkk.0331@gmail.com>
2025-01-09 11:51:53 +0900

    feat: 🎸 sp版のスタイル調整

--- src/pages/top/components/ReviewSlider/index.tsx
+++ src/pages/top/components/ReviewSlider/index.tsx
@@ -26,8 +26,9 @@ export const ReviewSlider = () => {
         <Image
           src={heroImage}
           alt={'利用者の車外観'}
-          width={165}
+          width={144}
           height={259}
+          className={styles.heroImage}
         />
       </div>
       <div className={styles.model}>SUZUKI Jimny</div>
