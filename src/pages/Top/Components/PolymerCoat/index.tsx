defa62a73853a237a173739b6a3cfd1c999557e0
gakki-san <gakki.nkk.0331@gmail.com>
2025-01-22 16:00:19 +0900

    refactor: 💡 background-imageをscssで当たるように変更

--- src/pages/Top/Components/PolymerCoat/index.tsx
+++ src/pages/Top/Components/PolymerCoat/index.tsx
@@ -3,22 +3,20 @@ import { ButtonLink } from '@/components/ButtonLink';
 
 const POLYMER_COAT_LINK =
   'https://www.dmm.co.jp/digital/videoa/-/detail/=/cid=h_1472bukgb00003/';
-const backgroundImage = '/assets/top/price/polymerCoatBackImage.jpg';
 
 export const PolymerCoat = () => {
   return (
-    <div
-      className={styles.container}
-      style={{
-        backgroundImage: `url(${backgroundImage})`,
-      }}
-    >
+    <div className={styles.container}>
       <h1 className={styles.polymerCoat}>ポリマーコート</h1>
       <h2 className={styles.minPrice}>50,000~</h2>
       <p className={styles.description}>
-        {
-          '※通常のコーティングではありません\n中古のMINIを買ったはいいけどコーティングはしたい\nだけどコーティングは高い\nそんな見えはりなあなたに！！'
-        }
+        ※通常のコーティングではありません
+        <br />
+        中古のMINIを買ったはいいけどコーティングはしたい
+        <br />
+        だけどコーティングは高い
+        <br />
+        そんな見えはりなあなたに！！
       </p>
       <div className={styles.button}>
         <ButtonLink link={POLYMER_COAT_LINK}>詳しい価格表を見る</ButtonLink>
