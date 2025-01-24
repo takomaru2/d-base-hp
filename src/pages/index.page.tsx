186cffbe01b807e317394ec0b0de1926cf5d4061
gakki-san <gakki.nkk.0331@gmail.com>
2025-01-24 21:38:10 +0900

    feat: 🎸 verticalTextまで完了。sp,pc含む

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,14 +1,18 @@
 import { Header } from '@/components/Header';
 import styles from './top/index.module.scss';
 import { TopView } from '@/pages/top/components/TopView';
+import { VerticalText } from '@/pages/top/components/VerticalText';
 
 export default function Top() {
   return (
     <>
-      <div className={styles.top}>
+      <div className={styles.hero}>
         <Header />
       </div>
       <TopView />
+      <div className={styles.vertical}>
+        <VerticalText />
+      </div>
     </>
   );
 }
