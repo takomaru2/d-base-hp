f3835ef3246346078a53ef75ea80ee6ff83affbd
gakki-san <gakki.nkk.0331@gmail.com>
2025-01-24 20:41:22 +0900

    feat: 🎸 topViewまでsp,pc完了

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,11 +1,14 @@
 import { Header } from '@/components/Header';
+import styles from './top/index.module.scss';
+import { TopView } from '@/pages/top/components/TopView';
 
 export default function Top() {
   return (
     <>
-      <div>
+      <div className={styles.top}>
         <Header />
       </div>
+      <TopView />
     </>
   );
 }
