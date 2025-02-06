8645fab312e3eb45745a05d5c20a0ea8512e1f91
gakki-san <gakki.nkk.0331@gmail.com>
2025-02-06 17:37:14 +0900

    refactor: 💡 components faq section

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,14 +1,13 @@
 import styles from './top/index.module.scss';
 import { TopView } from '@/pages/top/components/TopView';
 import { VerticalText } from '@/pages/top/components/VerticalText';
-import { SectionTitle } from '@/components/SectionTitle';
 import { PriceSection } from '@/pages/top/components/PriceSection';
-import { FAQContainer } from '@/pages/top/components/FAQContainer';
 import { AccessSection } from '@/pages/top/components/AccessSection';
 import { FixedBottomContact } from '@/components/FixedBottomContact';
 import { WorksSection } from '@/pages/top/components/WorksSection';
 import { LiquidSection } from '@/pages/top/components/LiquidSection';
 import { ReviewSection } from '@/pages/top/components/ReviewSection';
+import { FaqSection } from '@/pages/top/components/FaqSection';
 
 export default function Top() {
   return (
@@ -32,18 +31,7 @@ export default function Top() {
         <ReviewSection />
       </div>
 
-      <div className={styles.faq}>
-        <div className={`${styles.sectionTitle} ${styles.faqTitle}`}>
-          <SectionTitle
-            jpTitle={'よくあるご質問'}
-            enTitle={'Q ＆ A'}
-            color={'white'}
-          />
-        </div>
-        <div className={styles.faqContainer}>
-          <FAQContainer />
-        </div>
-      </div>
+      <FaqSection />
 
       <div className={styles.access}>
         <AccessSection />
