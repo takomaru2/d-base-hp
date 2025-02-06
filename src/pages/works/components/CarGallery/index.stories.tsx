07f66fbc0439e3eaab9c47b545b70763b4b81a86
gakki-san <gakki.nkk.0331@gmail.com>
2025-02-06 15:51:48 +0900

    refactor: 💡 maps cargallerylist

--- src/pages/works/components/CarGallery/index.stories.tsx
+++ src/pages/works/components/CarGallery/index.stories.tsx
@@ -1,7 +1,16 @@
 import { CarGallery as Component } from './index';
 import { Meta, StoryObj } from '@storybook/react';
 import styles from '@/pages/works/components/CarGallery/index.module.scss';
-import { swiperItemList01 } from '@/pages/works/components/CarGallery/const/carGalleryData';
+import image1 from '@/../public/assets/works/carGallery01.jpg';
+import image2 from '@/../public/assets/works/carGallery02.jpg';
+import image3 from '@/../public/assets/works/carGallery03.jpg';
+import { SwiperItem } from '@/pages/works/components/CarGallery/const/carGalleryData';
+
+const swiperItemList: SwiperItem[] = [
+  { id: '1', image: image1 },
+  { id: '2', image: image2 },
+  { id: '3', image: image3 },
+];
 
 const meta: Meta<typeof Component> = {
   component: Component,
@@ -9,7 +18,7 @@ const meta: Meta<typeof Component> = {
     layout: 'fullscreen',
   },
   args: {
-    swiperItemList: swiperItemList01,
+    swiperItemList: swiperItemList,
     model: 'TOYOTA HILUX',
     craft: 'NS10-H',
     size: 'LLサイズ',
--- src/pages/works/components/CarGallerySection/index.tsx
+++ src/pages/works/components/CarGallerySection/index.tsx
@@ -1,39 +1,23 @@
 import styles from './index.module.scss';
 import { CarGallery } from '@/pages/works/components/CarGallery';
-import {
-  swiperItemList01,
-  swiperItemList02,
-  swiperItemList03,
-  text,
-} from '@/pages/works/components/CarGallery/const/carGalleryData';
+import { carGalleryList } from '@/pages/works/components/CarGallery/const/carGalleryData';
 
 export const CarGallerySection = () => {
   return (
     <div className={styles.carGallery}>
-      <CarGallery
-        swiperItemList={swiperItemList01}
-        model={'TOYOTA　HILUX '}
-        craft={'NS10-H '}
-        size={'LLサイズ'}
-        price={'420,000円'}
-        text={text}
-      />
-      <CarGallery
-        swiperItemList={swiperItemList02}
-        model={'TOYOTA HARRIER '}
-        craft={'NS10-H '}
-        size={'LLサイズ'}
-        price={'550,000円'}
-        text={text}
-      />
-      <CarGallery
-        swiperItemList={swiperItemList03}
-        model={'LEXUS LC-500'}
-        craft={'NS10-H '}
-        size={'LLサイズ'}
-        price={'100,000円'}
-        text={text}
-      />
+      {carGalleryList.map(
+        ({ id, swiperItem, model, craft, size, price, text }) => (
+          <CarGallery
+            key={id}
+            swiperItemList={swiperItem}
+            model={model}
+            craft={craft}
+            size={size}
+            price={price}
+            text={text}
+          />
+        ),
+      )}
     </div>
   );
 };
