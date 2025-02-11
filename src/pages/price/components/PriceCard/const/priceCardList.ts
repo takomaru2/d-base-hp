479482b68ebfc95c61b81eaef5e2b406466d8fcc
gakki-san <gakki.nkk.0331@gmail.com>
2025-02-11 20:26:33 +0900

    feat: 🎸 create price card

---
+++ src/pages/price/components/PriceCard/const/priceCardList.ts
@@ -0,0 +1,37 @@
+export const priceCardList = [
+  {
+    id: '1',
+    size: 'SS',
+    price: '¥134,100',
+  },
+  {
+    id: '2',
+    size: 'S',
+    price: '¥134,100',
+  },
+  {
+    id: '3',
+    size: 'M',
+    price: '¥134,100',
+  },
+  {
+    id: '4',
+    size: 'L',
+    price: '¥134,100',
+  },
+  {
+    id: '5',
+    size: 'LL',
+    price: '¥134,100',
+  },
+  {
+    id: '6',
+    size: '3L',
+    price: '¥134,100',
+  },
+  {
+    id: '7',
+    size: '4L',
+    price: '¥134,100',
+  },
+];
