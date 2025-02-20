c571806be34a97563158a7ecb69e470572eeeeff
takomaru2 <col.dd93@gmail.com>
2025-02-20 17:32:17 +0900

    feat: 🎸 Add links to footerNav.ts and navMenu.ts

--- src/const/navMenu.ts
+++ src/const/navMenu.ts
@@ -7,5 +7,9 @@ export const navMenu: NavMenuList[] = [
   { id: '3', href: '/price', label: '価格表' },
   { id: '4', href: '#access', label: 'アクセス' },
   { id: '5', href: '#faq', label: 'よくある質問' },
-  { id: '6', href: 'https://form.run/@d-base-contact', label: 'お問い合わせ' },
+  {
+    id: '6',
+    href: 'https://form.run/@col-dd--9UTghEpb6KZmBlZ06Weo',
+    label: 'お問い合わせ',
+  },
 ];
