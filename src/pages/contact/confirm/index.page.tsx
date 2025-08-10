9eb8555cbb3e19fa0c9a5167cd02d7ec5bcac286
gakki-san <gakki.nkk.0331@gmail.com>
2025-08-10 18:18:42 +0900

    refactor: 💡 create loading message

--- src/pages/contact/confirm/index.page.tsx
+++ src/pages/contact/confirm/index.page.tsx
@@ -1,14 +1,12 @@
 import React, { FormEventHandler, useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import {
-  materialOptions,
-  periodOption,
-} from '@/pages/contact/const/contactOptions';
 import { UserInput } from '@/pages/contact/type';
 import { loadSessionData } from '@/pages/contact/confirm/logics/loadSession';
 import { ConfirmLayout } from '@/pages/contact/confirm/components/ConfirmLayout';
 import { CustomFetchType } from '@/types/customFetch';
 import { contactCustomFetch } from '@/pages/contact/logic/contactCustomFetch';
+import { formatConfirmField } from '@/pages/contact/confirm/logics/formatConfirmField';
+import { createLoadingMessage } from '@/pages/contact/confirm/logics/createLoadingMessage';
 
 export default function Confirm() {
   const [userInput, setUserInput] = useState<UserInput | undefined>();
@@ -19,12 +17,9 @@ export default function Confirm() {
     setUserInput(data);
   }, []);
 
-  if (!userInput)
-    return (
-      <p>
-        読み込み中です。時間がかかる場合は、リロードするか、入力しなおしてください
-      </p>
-    );
+  if (userInput === undefined) {
+    return createLoadingMessage();
+  }
 
   const onSubmit: FormEventHandler<HTMLFormElement> = async (event) => {
     event.preventDefault();
@@ -44,42 +39,10 @@ export default function Confirm() {
     router.back();
   };
 
-  const confirmFields = [
-    { id: 'name', label: 'お名前', value: userInput.name },
-    { id: 'katakana', label: 'フリガナ', value: userInput.katakana },
-    { id: 'mail', label: 'メールアドレス', value: userInput.mail },
-    { id: 'phone', label: 'ご連絡先', value: userInput.phone },
-    {
-      id: 'size',
-      label: '車種・サイズ',
-      value:
-        userInput['size'].length > 0 ? userInput['size'].join(', ') : '未選択',
-    },
-    {
-      id: 'period',
-      label: '車の年数',
-      value:
-        periodOption.find((option) => option.value === userInput.period)
-          ?.label ?? '未選択',
-    },
-    {
-      id: 'material',
-      label: '希望の液剤',
-      value:
-        materialOptions.find((option) => option.value === userInput.material)
-          ?.label ?? materialOptions[0].label,
-    },
-    {
-      id: 'postContent',
-      label: 'お問い合わせ内容',
-      value: userInput.postContent,
-    },
-  ];
-
   return (
     <ConfirmLayout
       onSubmit={onSubmit}
-      labels={confirmFields}
+      labels={formatConfirmField(userInput)}
       backButton={handleBackButton}
     />
   );
