1cdf8aa5211110908fb9ebab76bd78535bdceca4
gakki-san <gakki.nkk.0331@gmail.com>
2025-08-13 16:58:39 +0900

    refactor: 💡 rename api key

--- src/pages/api/send/route.api.ts
+++ src/pages/api/send/route.api.ts
@@ -5,7 +5,7 @@ import {
   periodOption,
 } from '@/pages/contact/const/contactOptions';
 
-const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY!);
+const resend = new Resend(process.env.RESEND_API_KEY!);
 
 export default async function handler(
   req: NextApiRequest,
