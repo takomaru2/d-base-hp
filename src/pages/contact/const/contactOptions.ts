5083d2d28ccc626e69c6d99ff75fc2951d37325d
gakki-san <gakki.nkk.0331@gmail.com>
2025-08-20 23:02:17 +0900

    fix: 🐛 Initial values not displayed

--- src/pages/contact/const/contactOptions.ts
+++ src/pages/contact/const/contactOptions.ts
@@ -42,7 +42,7 @@ export const initialInput = {
   mail: '',
   phone: '',
   size: [],
-  period: "'less_than_1_year'",
+  period: 'less_than_1_year',
   material: 'liquid_1',
   postContent: '',
 };
